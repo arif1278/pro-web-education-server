20558b24ce7b537a9d3b2a9de7ca77e73cb2444b
arif-hossain <manabethreeplusone@gmail.com>
2022-10-27 04:39:49 +0600

    sixth commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/course.json');
 
 app.get('/', (req, res) => {
     res.send('Growhand API Running');
@@ -13,7 +14,24 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_course = courses.find(c => c.category_id === id);
+    res.send(category_course);
 })
+
+app.get('/course', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectCourse = courses.find(c => c.id === id);
+    res.send(selectCourse);
+})
+
 
 app.listen(port, () => {
     console.log('Growhand news server running in port', port);
